bd5130f2cd8c81515198cc816b57fff7f0aeda3b
Nelson Cadenas <ncadenas@wingsoft.com>
2023-10-05 00:48:27 -0400

    95. Insertar multiples registros simultáneamente

--- README.md
+++ README.md
@@ -24,6 +24,12 @@ npm i -g @nestjs/cli
 docker-compose up -d
 ```
 
+5. Reconstruir la base de datos con la semilla:
+
+```
+GET http://localhost:3000/api/v2/seed
+```
+
 # Stack
 
 - Mongodb
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -14,16 +14,49 @@ export class SeedService {
   ) {}
 
   async executeSeed() {
+    // borrar las colecciones de pokemons primero
+    await this.pokemonModel.deleteMany({});
+
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
-    data.results.forEach(async ({ name, url }) => {
+    const pokemonToInsert: { name: string; no: number }[] = [];
+
+    data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
 
-      const pokemon = await this.pokemonModel.create({ no, name });
+      // const pokemon = await this.pokemonModel.create({ no, name });
+
+      pokemonToInsert.push({ no, name });
     });
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+    return 'Seed executed';
+  }
+
+  async executeSeedAnotherWay() {
+    // borrar las colecciones de pokemons primero
+    await this.pokemonModel.deleteMany({});
+
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    );
+
+    const insertPromisesArray = [];
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+
+      // const pokemon = await this.pokemonModel.create({ no, name });
+
+      insertPromisesArray.push(this.pokemonModel.create({ no, name }));
+    });
+
+    await Promise.all(insertPromisesArray);
 
     return 'Seed executed';
   }
