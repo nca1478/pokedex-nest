81b2ce4adf87e447be7bb9c45189a30ef89b7193
Nelson Cadenas <nca1478@yahoo.com>
2024-01-09 11:52:27 -0400

    109. Desplegar aplicación en la nube

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,8 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
+
+  console.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
